1ff88913761f5c8907f6bcc819db25186dba986a
gitci <git-ci@techinity.com>
2019-09-17 12:03:16 +0000

    release artifacts

---
+++ dist/changed-packages.d.ts
@@ -0,0 +1,6 @@
+/**
+ * Get the list of changed packages as a string
+ * @param cwd Current working directory
+ */
+export declare const changedPackages: (cwd: string) => Promise<string>;
+//# sourceMappingURL=changed-packages.d.ts.map
